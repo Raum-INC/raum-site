e4c814237a5c201ff264b828aec24900f1cd4331
Emmanuel Inua <artofnuel@gmail.com>
2023-09-18 19:53:07 +0100

    Update to Hero Component
    Included a Waitlist Modal. Under construction.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { Assets } from "../assets";
+import { IoMdClose } from "react-icons/io";
 
-const Hero = () => {
+const Hero = ({ setNav }) => {
+  const [modal, setModal] = useState(true | false);
+
+  const handleTrue = () => {
+    setModal(true);
+  };
+  const handleFalse = () => {
+    setModal(false);
+  };
+
+  const modalVariant = {
+    hidden: {
+      opacity: 0,
+      translateY: "100vh",
+      transition: {
+        duration: 1,
+      },
+    },
+    visible: {
+      opacity: 1,
+      translateY: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
   const componentVariant = {
     visible: {
       opacity: 1,
@@ -42,26 +68,95 @@ const Hero = () => {
       variants={componentVariant}
       initial="hidden"
       animate="visible"
-      className="w-full h-auto p-8 md:px-12 flex justify-center items-center relative"
+      className="w-full h-auto pt-8 flex justify-center items-center relative bgGrade2"
     >
-      <div className=" flex flex-col justify-center items-center gap-8">
-        <h1 className="text-4xl md:text-8xl text-center font-extrabold leading-8">
-          Discover <span className="text-primary">Your</span> Dream
-          <br /> Apartment with <span className="text-primary">Raum</span>
-        </h1>
-        <p className="md:w-[850px] text-center leading-5 text-fade text-sm md:text-2xl">
-          Finding the perfect apartment has never been easier. At Raum, we are
-          your dedicated partners in the world of real estate. Whether you're
-          searching for your next home or looking to list your apartment for
-          business purposes, we're here to make your journey seamless and
-          stress-free
-        </p>
-        <Link
-          to="joinus"
-          className="p-3 px-8 md:px-16 bg-primary rounded-3xl capitalize"
-        >
-          Join the future
-        </Link>
+      {/* modal */}
+      <AnimatePresence>
+        {modal === true && (
+          <>
+            <div className="w-full h-screen fixed top-20 overflow-hidden z-30">
+              <motion.div
+                variants={modalVariant}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                className="w-full h-[90%] fixed flex flex-col bg-black border-2 border-primary p-4 md:p-8 md:px-12 rounded-3xl z-40"
+              >
+                <div className="relative">
+                  <button
+                    className="p-2 absolute right-0 border-2 border-primary text-primary hover:bg-primary hover:text-white rounded-lg transition-all duration-500"
+                    onClick={handleFalse}
+                  >
+                    <IoMdClose className="text-xl md:text-4xl" />
+                  </button>
+                  <form action="/">
+                    <div className="flex flex-col justify-center items-center pt-2 md:pt-8 gap-5">
+                      <h2 className="mt-10 font-semibold text-primary text-xl lg:text-6xl md:leading-[108px] text-center p-2">
+                        Join us in creating sustainable spaces
+                        <br className="hidden md:block" /> for life, not just
+                        living.
+                      </h2>
+                      <p className="font-[400] text-sm text-center md:text-xl">
+                        Subscribe to our newsletter and be part of the dawn of a
+                        new era.
+                      </p>
+                      <div className="flex flex-col gap-2 md:gap-5 mt-5">
+                        <input
+                          type="text"
+                          className="bg-[#121212] p-2 md:p-4 outline-none pl-5 text-xl text-[#666] rounded-md"
+                          placeholder="Full Name"
+                        />
+                        <input
+                          type="text"
+                          className="bg-[#121212] p-2 md:p-4 outline-none pl-5 text-xl text-[#666] rounded-md"
+                          placeholder="Full Name"
+                        />
+                        <input
+                          type="text"
+                          className="bg-[#121212] p-2 md:p-4 outline-none pl-5 text-xl text-[#666] rounded-md"
+                          placeholder="Full Name"
+                        />
+                        <input
+                          type="text"
+                          className="bg-[#121212] p-2 md:p-4 outline-none pl-5 text-xl text-[#666] rounded-md"
+                          placeholder="Full Name"
+                        />
+                        <input
+                          type="submit"
+                          value="Join Us Today!"
+                          onClick={handleFalse}
+                          className="bg-primary mt-4 p-2 md:p-4 rounded-md"
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </motion.div>
+            </div>
+          </>
+        )}
+      </AnimatePresence>
+
+      <div className="w-full h-auto flex flex-col justify-center items-center gap-8">
+        <div className="px-8 flex flex-col justify-center items-center gap-8">
+          <h1 className="text-4xl md:text-8xl text-center font-extrabold leading-8">
+            Discover <span className="text-primary">Your</span> Dream
+            <br /> Apartment with <span className="text-primary">Raum</span>
+          </h1>
+          <p className="md:w-[850px] text-center leading-5 text-fade text-sm md:text-2xl">
+            Finding the perfect apartment has never been easier. At Raum, we are
+            your dedicated partners in the world of real estate. Whether you're
+            searching for your next home or looking to list your apartment for
+            business purposes, we're here to make your journey seamless and
+            stress-free
+          </p>
+          <button
+            className="p-3 px-8 md:px-16 bg-primary rounded-3xl capitalize"
+            onClick={handleTrue}
+          >
+            Join the Waitlist
+          </button>
+        </div>
         <div>
           <img
             src={Assets.hero_bg1}
@@ -84,10 +179,10 @@ const Hero = () => {
         >
           <img
             src={Assets.hero_bg3}
-            alt="image"
-            className="md:w-4/5 mx-auto aspect-auto pt-10 md:absolute bottom-4 -z-[10]"
+            alt="hero-bkg"
+            className="md:w-[1000px] mx-auto aspect-auto pt-10 px-8 md:absolute bottom-4 -z-[10]"
           />
-          <div className="bg-black w-full h-auto p-6 flex flex-col justify-center items-center gap-8">
+          <div className="w-full h-auto bg-black border border-primary p-6 flex flex-col justify-center items-center gap-8">
             <h1 className="text-3xl font-semibold">Who are you?</h1>
             <div className="flex flex-col gap-5 md:flex-row">
               <button className="p-3 px-5 rounded-full border border-primary hover:bg-primary hover:tex-white transition-all duration-500">
